352aa47594f6288dedeb2704015d3a65b76f1ca8
Abdurrahman Javat <a.javat70@gmail.com>
2024-08-20 20:40:06 +0300

    created in/out zones

--- main.py
+++ main.py
@@ -3,8 +3,35 @@ import numpy as np
 import supervision as sv
 from ultralytics import YOLO
 import cv2
+from typing import Dict, Iterable, List, Set
 
 COLORS = sv.ColorPalette.DEFAULT
+
+ZONE_IN_POLYGONS = [
+    np.array([[592, 282], [900, 282], [900, 82], [592, 82]]),
+    np.array([[950, 860], [1250, 860], [1250, 1060], [950, 1060]]),
+    np.array([[592, 582], [592, 860], [392, 860], [392, 582]]),
+    np.array([[1250, 282], [1250, 530], [1450, 530], [1450, 282]]),
+]
+
+ZONE_OUT_POLYGONS = [
+    np.array([[950, 282], [1250, 282], [1250, 82], [950, 82]]),
+    np.array([[592, 860], [900, 860], [900, 1060], [592, 1060]]),
+    np.array([[592, 282], [592, 550], [392, 550], [392, 282]]),
+    np.array([[1250, 860], [1250, 560], [1450, 560], [1450, 860]]),
+]
+
+def initiate_polygon_zones(
+    polygons: List[np.ndarray],
+    triggering_anchors: Iterable[sv.Position] = [sv.Position.CENTER],
+) -> List[sv.PolygonZone]:
+    return [
+        sv.PolygonZone(
+            polygon=polygon,
+            triggering_anchors=triggering_anchors,
+        )
+        for polygon in polygons
+    ]
 
 class VideoProcessor:
     def __init__(self,
@@ -18,6 +45,10 @@ class VideoProcessor:
         self.target_video_path = target_video_path
         self.confidence_threshold = confidence_threshold
         self.iou_threshold = iou_threshold
+        
+        self.video_info = sv.VideoInfo.from_video_path(source_video_path)
+        self.zones_in = initiate_polygon_zones(ZONE_IN_POLYGONS, [sv.Position.CENTER])
+        self.zones_out = initiate_polygon_zones(ZONE_OUT_POLYGONS, [sv.Position.CENTER])
         
         self.model = YOLO(source_weights_path)
         self.tracker = sv.ByteTrack()
@@ -46,6 +77,20 @@ class VideoProcessor:
         annotated_frame = self.label_annotator.annotate(
             annotated_frame, detections, labels
         )
+        
+        for zone_out_id, zone_out in enumerate(self.zones_out):
+            zone_center = sv.get_polygon_center(polygon=zone_out.polygon)
+            if zone_out_id in self.detections_manager.counts:
+                counts = self.detections_manager.counts[zone_out_id]
+                for i, zone_in_id in enumerate(counts):
+                    count = len(self.detections_manager.counts[zone_out_id][zone_in_id])
+                    text_anchor = sv.Point(x=zone_center.x, y=zone_center.y + 40 * i)
+                    annotated_frame = sv.draw_text(
+                        scene=annotated_frame,
+                        text=str(count),
+                        text_anchor=text_anchor,
+                        background_color=COLORS.colors[zone_in_id],
+                    )
         return annotated_frame
     
     def process_frame(self, frame: np.ndarray) -> np.ndarray:
